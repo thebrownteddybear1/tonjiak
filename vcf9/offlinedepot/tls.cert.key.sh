73cb29164d0918388d3e63693f23af3923a28b9f
teddy <thebrownteddybear@gmail.com>
2025-12-25 12:08:11 +0000

    asdf

--- vcf9/offlinedepot/tls.cert.key.sh
+++ vcf9/offlinedepot/tls.cert.key.sh
@@ -27,6 +27,6 @@ openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -config server_openssl.cnf
 openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.pem -subj "/C=US/ST=CA/L=Palo Alto/O=WilliamLam/OU=R&D/CN=WilliamLam-RootCA"
 openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 825 -sha256 -extensions req_ext -extfile server_openssl.cnf
-cat server.crt rootCA.perm > depot-fullchain.pem
-python3 http_server_auth.py --bind 192.168.50.53 --user vcf --password vcf123! --port 443 --directory /root/tonjiak/vcf9/offlinedepot \
- --certfile server.crt --keyfile server.key
+cat server.crt rootCA.pem > depot-fullchain.pem
+python3 http_server_auth.py --user vcf --password VMware1!VMware1! --port 443 --directory /root/tonjiak/vcf9/offlinedepot \
+ --certfile server.crt --keyfile server.key 
