891525f6c94b649331d8e1c0e9f33c76fbdb0281
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-01-22 20:16:58 +0800

    asdf

--- vcf9/offlinedepot/4webserver.sh
+++ vcf9/offlinedepot/4webserver.sh
@@ -1,2 +1,2 @@
-cdpython3 http_server_auth.py --bind 0.0.0.0 --user root --password "VMware1!VMware1!" --port 443 --directory /root/tonjiak/vcf9/offlinedepot/depot/vcf9.01 \
---certfile ~/tonjiak/vcf9/offlinedepot/playbook/server.crt --keyfile ~/tonjiak/vcf9/offlinedepot/playbook/server.key &
+python3 http_server_auth.py --bind 0.0.0.0 --user root --password "VMware1!VMware1!" --port 443 --directory /root/tonjiak/vcf9/offlinedepot/depot/vcf9.01 \
+--certfile ~/tonjiak/vcf9/offlinedepot/server.crt --keyfile ~/tonjiak/vcf9/offlinedepot/server.key &
