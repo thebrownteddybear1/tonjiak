6a76b3555406fe353a7a3a12830f9de08bb1919b
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-02-14 23:13:24 +0800

    aasf

--- bash-wcpcli9-multi-az/enable-wcp.sh
+++ bash-wcpcli9-multi-az/enable-wcp.sh
@@ -53,11 +53,16 @@ K8S_STORAGE_POLICY='wk1sc2'
 export NSX_EDGE_CLUSTER='edge-cluster-1'
 export NSX_T0_GATEWAY='tier0-gateway'
 export NSX_DVS_PORTGROUP='cluster1-vds-01'
-export NSX_INGRESS_NW='10.220.3.1'
-export NSX_INGRESS_COUNT=512
-export NSX_EGRESS_NW='10.220.30.1'
-export NSX_EGRESS_COUNT=512
-export NSX_NAMESPACE_NW='10.244.0.0'
+
+# Change Ingress to a CIDR (e.g., /28 or /29)
+export NSX_INGRESS_NW='10.220.3.0/24'
+export NSX_INGRESS_COUNT=200
+
+# Change Egress to a CIDR (Minimum /29 required for 4+ IPs)
+export NSX_EGRESS_NW='10.220.30.0/24' 
+export NSX_EGRESS_COUNT=200
+
+export NSX_NAMESPACE_NW='10.244.0.0/16'
 export NSX_NAMESPACE_COUNT=4096
 
 #############################################################
