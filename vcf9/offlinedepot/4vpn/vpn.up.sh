2fa43a7ed8cd97d0093e7cfce62f4438b0a27b82
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-05-20 13:18:27 +0800

    asdf

--- vcf9/offlinedepot/4vpn/vpn.up.sh
+++ vcf9/offlinedepot/4vpn/vpn.up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-openconnect -u 'ty036880@broadcom.net'      --protocol=gp --csd-wrapper /usr/libexec/openconnect/hipreport.sh      portal.vpn.broadcom.com -b
+openconnect  --protocol=gp --csd-wrapper /usr/libexec/openconnect/hipreport.sh      portal.vpn.broadcom.com -b
